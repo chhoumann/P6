08e5bcde55fef99273fe4848d78000641097151c
Pattrigue <pfas19@student.aau.dk>
2022-05-02 11:24:02 +0200

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,7 +1,13 @@
 from data.get_data import DataType, get_data
-
+from transformer.transformer import Transformer
 
 if __name__ == "__main__":
     print("Starting")
     data = get_data(DataType.delhi_small)
-    print(data["X_train"])
+
+    transformer = Transformer(n_encoder_inputs=8, n_decoder_inputs=9, d_model=512, nhead=8)
+    pred = transformer((data["X_train"], data["y_train"]))
+
+    print(pred)
+
+    #print(data["X_train"])
