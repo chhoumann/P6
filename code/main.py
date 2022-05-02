b2e7d5906b69c77ca81b7da8b273773766eaa8d1
Pattrigue <pfas19@student.aau.dk>
2022-05-02 14:28:38 +0200

    comedy central

--- code/main.py
+++ code/main.py
@@ -1,18 +1,36 @@
 from data.get_data import DataType, get_data
 from transformer.transformer import Transformer
+from transformer.Time2Vector import Time2Vector
+import tensorflow as tf
 
 import torch
+
+def RMSELoss(yhat,y):
+    return torch.sqrt(torch.mean((yhat-y)**2))
 
 if __name__ == "__main__":
     print("Starting")
     data = get_data(DataType.delhi_small)
 
-    transformer = Transformer(1452, 10)
     # src = torch.LongTensor([[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]]).view(10, 1)
     # print(data["X_train"][0].view(10, 1))
-    print(data["X_train"])
-    x = torch.tensor(data["X_train"]).to(torch.int64)
+    # print(data["X_train"])
+
+
+    time_embedding = Time2Vector(10)
+    x = data["X_train"]
+    # x = torch.tensor(x).to(torch.int64)
+    # x = tf.cast(x, tf.float32)
+    x = tf.convert_to_tensor(x, dtype=tf.float32)
+    x = time_embedding(x)
+    x = x.numpy()
+    x = torch.tensor(x)
+
     print(x)
-    # transformer(x)
+    transformer = Transformer(1452, 2)
+    pred = transformer(x)
     # print(pred)
+    criterion = RMSELoss
+    loss = criterion(pred, data["y_train"])
+    print(loss)
 
--- code/transformer/transformer.py
+++ code/transformer/transformer.py
@@ -6,15 +6,16 @@ class Transformer(nn.Module):
     def __init__(self, vocab_size, feature_size):
         super(Transformer, self).__init__()
 
-        self.embedding = nn.Embedding(vocab_size, feature_size)
-        self.layers = nn.TransformerEncoderLayer(d_model=feature_size, nhead=10)
+        # self.embedding = nn.Embedding(1452, 10)
+        self.layers = nn.TransformerEncoderLayer(d_model=feature_size, nhead=1)
         self.transformer = nn.TransformerEncoder(self.layers, num_layers=6)
         self.decoder = nn.Linear(feature_size, vocab_size)
 
     def forward(self, x):
         # X shape: [seq_len, batch_size]
         print(x.shape)
-        x = self.embedding(x)
+
+        # x = self.embedding(x)
         # X shape: [seq_len, batch_size, feature_size]
         print(x.shape)
         x = self.transformer(x)
