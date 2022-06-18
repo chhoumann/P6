6c8da93aea0381762923adb9e7d8d6d4101bfac0
Christian Bager Bach Houmann <christian@bagerbach.com>
2022-06-18 11:20:59 +0200

    transformer (last minute, not refactored) impl. for web-app

--- code/web-app/app.py
+++ code/web-app/app.py
@@ -2,6 +2,7 @@ import streamlit
 from classes.lstm import LSTMModel, Optimization
 from classes.gru import GRUModel
 from classes.rnn import RNNModel
+from classes.transformer import *
 from rmse import RMSELoss
 from statsmodels.tsa.arima.model import ARIMA
 from predict_page import show_predict_page
---
+++ code/web-app/classes/transformer.py
@@ -0,0 +1,183 @@
+import numpy as np
+import pandas as pd
+import os, datetime
+import tensorflow as tf
+from tensorflow.keras.models import *
+from tensorflow.keras.layers import *
+from keras import backend as K
+from tensorflow import keras
+from tensorflow.keras import layers
+
+batch_size = 10
+seq_len = 20
+
+d_k = 40
+d_v = 40
+n_heads = 6
+ff_dim = 40
+
+LAG=10
+
+class Time2Vector(Layer):
+    def __init__(self, seq_len, **kwargs):
+        super(Time2Vector, self).__init__()
+        self.seq_len = seq_len
+
+    def build(self, input_shape):
+        '''Initialize weights and biases with shape (batch, seq_len)'''
+        self.weights_linear = self.add_weight(name='weight_linear',
+                                              shape=(int(self.seq_len),),
+                                              initializer='uniform',
+                                              trainable=True)
+
+        self.bias_linear = self.add_weight(name='bias_linear',
+                                           shape=(int(self.seq_len),),
+                                           initializer='uniform',
+                                           trainable=True)
+
+        self.weights_periodic = self.add_weight(name='weight_periodic',
+                                                shape=(int(self.seq_len),),
+                                                initializer='uniform',
+                                                trainable=True)
+
+        self.bias_periodic = self.add_weight(name='bias_periodic',
+                                             shape=(int(self.seq_len),),
+                                             initializer='uniform',
+                                             trainable=True)
+
+    def call(self, x):
+        '''Calculate linear and periodic time features'''
+        x = tf.math.reduce_mean(x[:, :, :LAG], axis=-1)
+        time_linear = self.weights_linear * x + self.bias_linear  # Linear time feature
+        time_linear = tf.expand_dims(time_linear, axis=-1)  # Add dimension (batch, seq_len, 1)
+
+        time_periodic = tf.math.sin(tf.multiply(x, self.weights_periodic) + self.bias_periodic)
+        time_periodic = tf.expand_dims(time_periodic, axis=-1)  # Add dimension (batch, seq_len, 1)
+        return tf.concat([time_linear, time_periodic], axis=-1)  # shape = (batch, seq_len, 2)
+
+    def get_config(self):  # Needed for saving and loading model with custom layer
+        config = super().get_config().copy()
+        config.update({'seq_len': self.seq_len})
+        return config
+
+
+class SingleAttention(Layer):
+    def __init__(self, d_k, d_v):
+        super(SingleAttention, self).__init__()
+        self.d_k = d_k
+        self.d_v = d_v
+
+    def build(self, input_shape):
+        self.query = Dense(self.d_k,
+                           input_shape=input_shape,
+                           kernel_initializer='glorot_uniform',
+                           bias_initializer='glorot_uniform')
+
+        self.key = Dense(self.d_k,
+                         input_shape=input_shape,
+                         kernel_initializer='glorot_uniform',
+                         bias_initializer='glorot_uniform')
+
+        self.value = Dense(self.d_v,
+                           input_shape=input_shape,
+                           kernel_initializer='glorot_uniform',
+                           bias_initializer='glorot_uniform')
+
+    def call(self, inputs):  # inputs = (in_seq, in_seq, in_seq)
+        q = self.query(inputs[0])
+        k = self.key(inputs[1])
+
+        attn_weights = tf.matmul(q, k, transpose_b=True)
+        attn_weights = tf.map_fn(lambda x: x / np.sqrt(self.d_k), attn_weights)
+        attn_weights = tf.nn.softmax(attn_weights, axis=-1)
+
+        v = self.value(inputs[2])
+        attn_out = tf.matmul(attn_weights, v)
+        return attn_out
+
+class MultiAttention(Layer):
+  def __init__(self, d_k, d_v, n_heads):
+    super(MultiAttention, self).__init__()
+    self.d_k = d_k
+    self.d_v = d_v
+    self.n_heads = n_heads
+    self.attn_heads = list()
+
+  def build(self, input_shape):
+    for n in range(self.n_heads):
+      self.attn_heads.append(SingleAttention(self.d_k, self.d_v))
+    self.linear = Dense(LAG + 3, input_shape=input_shape, kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform')
+
+  def call(self, inputs):
+    attn = [self.attn_heads[i](inputs) for i in range(self.n_heads)]
+    concat_attn = tf.concat(attn, axis=-1)
+    multi_linear = self.linear(concat_attn)
+    return multi_linear
+
+
+class TransformerEncoder(Layer):
+    def __init__(self, d_k, d_v, n_heads, ff_dim, dropout=0.3, **kwargs):
+        super(TransformerEncoder, self).__init__()
+        self.d_k = d_k
+        self.d_v = d_v
+        self.n_heads = n_heads
+        self.ff_dim = ff_dim
+        self.attn_heads = list()
+        self.dropout_rate = dropout
+
+    def build(self, input_shape):
+        self.attn_multi = MultiAttention(self.d_k, self.d_v, self.n_heads)
+        self.attn_dropout = Dropout(self.dropout_rate)
+        self.attn_normalize = LayerNormalization(input_shape=input_shape, epsilon=1e-7)
+        self.ff_conv1D_1 = Conv1D(filters=self.ff_dim, kernel_size=1, activation='relu',
+                                  kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform')
+        self.ff_conv1D_2 = Conv1D(filters=LAG + 3, kernel_size=1, kernel_initializer='glorot_uniform',
+                                  bias_initializer='glorot_uniform')  # input_shape[0]=(batch, seq_len, 8), input_shape[0][-1]=8
+        self.ff_dropout = Dropout(self.dropout_rate)
+        self.ff_normalize = LayerNormalization(input_shape=input_shape, epsilon=1e-7)
+
+    def call(self, inputs):  # inputs = (in_seq, in_seq, in_seq)
+        attn_layer = self.attn_multi(inputs)
+        attn_layer = self.attn_dropout(attn_layer)
+        attn_layer = self.attn_normalize(inputs[0] + attn_layer)
+
+        ff_layer = self.ff_conv1D_1(attn_layer)
+        ff_layer = self.ff_conv1D_2(ff_layer)
+        ff_layer = self.ff_dropout(ff_layer)
+        ff_layer = self.ff_normalize(inputs[0] + ff_layer)
+        return ff_layer
+
+    def get_config(self):  # Needed for saving and loading model with custom layer
+        config = super().get_config().copy()
+        config.update({'d_k': self.d_k,
+                       'd_v': self.d_v,
+                       'n_heads': self.n_heads,
+                       'ff_dim': self.ff_dim,
+                       'attn_heads': self.attn_heads,
+                       'dropout_rate': self.dropout_rate})
+        return config
+
+
+def create_model():
+    time_embedding = Time2Vector(seq_len)
+    attn_layer1 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+    attn_layer2 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+    attn_layer3 = TransformerEncoder(d_k, d_v, n_heads, ff_dim)
+
+    in_seq = Input(shape=(seq_len, LAG + 1))
+    x = time_embedding(in_seq)
+    x = Concatenate(axis=-1)([in_seq, x])
+    x = attn_layer1((x, x, x))
+    x = attn_layer2((x, x, x))
+    x = attn_layer3((x, x, x))
+    x = GlobalAveragePooling1D(data_format='channels_first')(x)
+    x = Dropout(0.3)(x)
+    x = Dense(128, activation='relu')(x)
+    x = Dropout(0.3)(x)
+    out = Dense(1, activation='linear')(x)
+
+    model = Model(inputs=in_seq, outputs=out)
+    optimizer = keras.optimizers.Adam(learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-07)
+    # optimizer = keras.optimizers.SGD(learning_rate=0.02, momentum=0.9, nesterov=True, clipnorm=1.0, clipvalue=0.5)
+    model.compile(loss='mse', optimizer=optimizer, metrics=['mae'])
+    return model
